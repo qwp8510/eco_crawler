4cbc98ca9fe17ff6a274b56447386cfeb694296f
Daniel Chiang <44150378+qwp8510@users.noreply.github.com>
2021-02-07 16:20:07 +0800

    add line notify

---
+++ src/notificaions/line_notify.py
@@ -0,0 +1,14 @@
+import requests
+
+
+class LineNotification():
+    LINE_URL = 'https://notify-api.line.me/api/notify'
+
+    def __init__(self, line_token):
+        self._line_token = line_token
+
+    def notify(self, msg):
+        headers = {'Authorization': 'Bearer ' + self._line_token}
+        payload = {'message': str(msg)}
+        response = requests.post(self.LINE_URL, headers=headers, params=payload)
+        return response.status_code
